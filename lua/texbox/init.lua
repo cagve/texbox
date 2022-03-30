48776713cc3e078b5ccb0d0e601a1c2e47870917
Carlos Aguilera Ventura <carlos.aguilera13416@gmail.com>
2022-03-31 00:46:42 +0200

    [FEAT] get_visual_text

--- lua/texbox/init.lua
+++ lua/texbox/init.lua
@@ -2,7 +2,6 @@ package.loaded['texbox'] = nil
 package.loaded['texbox.latex'] = nil
 package.loaded['texbox.telescope_latex'] = nil
 
-local telescope = require('texbox.telescope_latex')
 local latex = require('texbox.latex')
 
 function table.merge(t1, t2)
