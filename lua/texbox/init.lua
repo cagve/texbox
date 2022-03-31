586f7053f42ba4dc43dd2daf3bda8843f760964f
Carlos Aguilera Ventura <carlos.aguilera13416@gmail.com>
2022-03-31 14:00:25 +0200

    [FEAT] extract_visual_text; [FIX] extract_section

--- lua/texbox/init.lua
+++ lua/texbox/init.lua
@@ -2,23 +2,3 @@ package.loaded['texbox'] = nil
 package.loaded['texbox.latex'] = nil
 package.loaded['texbox.telescope_latex'] = nil
 
-local latex = require('texbox.latex')
-
-function table.merge(t1, t2)
-   for _,v in ipairs(t2) do
-      table.insert(t1, v)
-   end
-   return t1
-end
-
-local function extract_section()
-	local preamble = latex.get_preamble()
-	local document = latex.get_section()
-	local text = table.merge(preamble,document)
-	latex.create_document(text)
-end
-
-return {
-	extract_section = extract_section
-}
-
--- plugin/texbox.vim
+++ plugin/texbox.vim
@@ -5,7 +5,7 @@ let g:loaded_texbox=1
 " Comandos
 command! TXtelescopelabels lua require'texbox.telescope_latex'.labels_telescope()
 command! TXaddlabels lua require'texbox.latex'.add_labels()
-command! TXextractsection lua require'texbox'.extract_section()
+command! TXextractsection lua require'texbox.latex'.extract_section()
 command! TXnewcommand lua require'texbox.latex'.new_command()
 command! TXaddconceal lua require'texbox.latex'.add_conceal()
 
